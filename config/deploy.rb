265fc77ff491fc0636a0e746c76743e7df02735c
Sean Lerner <sean@smallcity.ca>
2016-07-06 10:28:28 -0400

    switch to ruby 2.3.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :linked_files, %w(config/database.yml .env)
 set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.2.3'
+set :rbenv_ruby, '2.3.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all
