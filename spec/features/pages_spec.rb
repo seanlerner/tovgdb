16abd112da0312bedaa6b635b0a03ffb721fa6fc
Sean Lerner <sean@smallcity.ca>
2016-07-07 11:40:52 -0400

    travis test

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,6 +1,10 @@
 describe Page do
   let_and_create! :game
 
+  it 'fails for testing travis' do
+    expect(false).to eq true
+  end
+
   it 'views home' do
     visit root_path
     expect(page).to have_content 'Recently Added Games'
