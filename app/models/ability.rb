d23f89258a6d4cadca36b3419419f918111726f9
Sean Lerner <sean@smallcity.ca>
2016-07-09 16:42:04 -0400

    removed all from clerk rights

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,6 @@ class Ability
       can :manage, :all
     else
       can :read, ActiveAdmin::Page, name: 'Dashboard'
-      can :manage, :all
       ([Game, Creator, DistributionChannel, LinkType] + Game::TAGS).each do |tovgdb_model|
         can :manage, tovgdb_model
       end
