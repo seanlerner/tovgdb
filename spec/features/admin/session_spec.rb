511dd19d89e1d1e002b9aa979f0244c7c9e18c1c
Sean Lerner <sean@smallcity.ca>
2016-07-10 16:46:50 -0400

    test for redirect for non-logged-in user

--- spec/features/admin/session_spec.rb
+++ spec/features/admin/session_spec.rb
@@ -18,4 +18,15 @@ describe 'the signin process' do
     click_link 'Logout'
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
+
+  it 'admin dashboard redirects to the login page for non-logged in visitor' do
+    visit admin_dashboard_path
+    expect(current_path).to eq new_admin_user_session_path
+  end
+
+  it 'does not allow clerk to view admin users' do
+    login_as create(:admin_user, :clerk)
+    visit admin_admin_users_path
+    expect(current_path).to eq admin_dashboard_path
+  end
 end
