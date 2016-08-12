b9fdaac4e1ccea7d5ecf9c363711bf9d3fa852cc
Sean Lerner <sean@smallcity.ca>
2016-08-12 11:32:40 -0400

    clerk sees same dashboard as admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,14 +1,6 @@
 ActiveAdmin.register_page 'Dashboard' do
   menu priority: -200
-
   content do
-    case current_admin_user.role
-    when 'Super Admin'
-      render partial: 'games_by_publication_statuses', locals:
-        { publication_statuses: [:processed_complete, :processed_incomplete, :published_incomplete,
-                                 :published_complete, :ready_for_processing, :processing, :undefined] }
-    when 'Clerk'
-      render partial: 'games_by_publication_statuses', locals: { publication_statuses: [:ready_for_processing] }
-    end
+    render partial: 'dashboard'
   end
 end
