47e6f11b33c9dd7dfb813e3b2d935a16a05f3d87
Sean Lerner <sean@smallcity.ca>
2017-09-24 22:00:52 -0400

    bullet ignore

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -2,4 +2,5 @@ if Rails.env.development?
   Bullet.enable = true
   Bullet.bullet_logger = true
   Bullet.add_footer = true
+  Bullet.add_whitelist type: :unused_eager_loading, class_name: 'Game', association: :creators
 end
