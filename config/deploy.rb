642b1d498c18d32c86a8373a585a88a5e1003b2f
Sean Lerner <sean@smallcity.ca>
2017-09-25 11:23:27 -0400

    config for staging env

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,45 +4,22 @@ require 'dotenv'
 
 Dotenv.load('~/.local/tovgdb/.env')
 
-set :application, 'tovgdb'
-set :repo_url, 'git@github.com:seanlerner/tovgdb.git'
-set :rbenv_path, '/home/deploy/.rbenv/'
+set :stages,          ['staging', 'production']
+set :default_stage,   'staging'
+set :application,     'tovgdb'
+set :repo_url,        'git@github.com:seanlerner/tovgdb.git'
+set :rbenv_path,      '/home/deploy/.rbenv/'
 
-set :deploy_to, '/home/deploy/tovgdb'
-
-set :linked_files, %w[config/database.yml .env]
-set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system]
+set :linked_files,    %w[config/database.yml .env]
+set :linked_dirs,     %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system]
 set :bundle_binstubs, nil
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.4.2'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w[rake gem bundle ruby rails]
-set :rbenv_roles, :all
+set :rbenv_type,      :user
+set :rbenv_ruby,      '2.4.2'
+set :rbenv_prefix,    "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins,  %w[rake gem bundle ruby rails]
+set :rbenv_roles,     :all
 
-set :keep_releases, 20
-set :format, :pretty
-set :log_level, :info
-
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  before :starting, :ensure_user do
-    git_repo = Git.open(Dir.pwd)
-    if git_repo.status.changed != {}
-      message = 'Changes to code must be committed first before deploying.'.red
-      abort(message)
-    elsif git_repo.revparse('HEAD') != git_repo.revparse('origin/master')
-      message = 'Changes have been commited but not pushed. Please push first before deploying.'.red
-      abort(message)
-    end
-  end
-
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
-end
+set :keep_releases,   20
+set :format,          :pretty
+set :log_level,       :info
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,25 +4,14 @@ role :db,  "#{ENV['CAPISTRANO_USER']}@#{ENV['CAPISTRANO_SERVER']}"
 
 server ENV['CAPISTRANO_SERVER'], user: ENV['CAPISTRANO_USER'], roles: %w[web app]
 
-set :stage,     :production
-set :deploy_to, '/home/deploy/tovgdb'
+set :stage,     :staging
+set :deploy_to, '/home/deploy/staging'
 
 namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  before :starting, :ensure_user do
-    git_repo = Git.open(Dir.pwd)
-    if git_repo.status.changed != {}
-      message = 'Changes to code must be committed first before deploying.'.red
-      abort(message)
-    elsif git_repo.revparse('HEAD') != git_repo.revparse('origin/master')
-      message = 'Changes have been commited but not pushed. Please push first before deploying.'.red
-      abort(message)
     end
   end
 
